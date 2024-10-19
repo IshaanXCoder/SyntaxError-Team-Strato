3f0e2a32b5ac2ea80d0ff2fd674395a5af129577
Ansh Karwasra <anshkarwasra15@gmail.com>
2024-10-19 20:36:02 +0530

    finalTestUi

--- testui/src/App.jsx
+++ testui/src/App.jsx
@@ -4,32 +4,14 @@ import axios from 'axios'
 import ChatUi from './components/chatUI'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
   // const [prompt, setprompt] = useState('')
   // const [response, setresponse] = useState('')
-  // const handleOnchange = (e) => {
-  //   console.log(e.target.value)
-  //     setprompt(e.target.value)
-  // }
-  // const handleClick = () => {
-  //   const formData = new FormData();
-  //   formData.append('prompt', prompt);
-  //   console.log(formData)
-  //   axios(
-  //     {
-  //       method: 'post',
-  //       url: 'http://localhost:5000/AI',
-  //       data: formData,
-  //       headers: { 'Content-Type': 'multipart/form-data' },
-  //     }
-  //   ).then((res) => {
-  //     console.log(res.data)
-  //     setresponse(res.data)
-  //   })
-  // }
+  
   return (
     <>
     <ChatUi/>
+    
     </>
   )
 }
